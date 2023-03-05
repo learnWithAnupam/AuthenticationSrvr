178167e087fa513faed5b9e82d5291f0c7f8a06b
learnWithAnupam <124241246+learnWithAnupam@users.noreply.github.com>
2023-03-05 22:51:54 +0530

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO user(id,first_name, last_name, email, password) VALUES( 1, 'Anupam', 'Mishra', 'anupamhmisra@gmail.com','$2a$04$LqDKRYWVlGhjfvOjdkfYmunwBHxrO0XkklSga09bYfY8K0/wZwVKq');
-INSERT INTO user(id,first_name, last_name, email, password) VALUES( 2, 'Shefali', 'Mishra', 'shefalihmisra@gmail.com','$2a$04$LqDKRYWVlGhjfvOjdkfYmunwBHxrO0XkklSga09bYfY8K0/wZwVKq');
+INSERT INTO user(id,first_name, last_name, email, password) VALUES( 1, 'Anupam', 'Mishra', 'anupamhm@gmail.com','$2a$04$LqDKRYWVlGhjfvOjdkfYmunwBHxrO0XkklSga09bYfY8K0/wZwVKq');
+INSERT INTO user(id,first_name, last_name, email, password) VALUES( 2, 'Ashish', 'Mishra', 'ashishhm@gmail.com','$2a$04$LqDKRYWVlGhjfvOjdkfYmunwBHxrO0XkklSga09bYfY8K0/wZwVKq');
 
 INSERT INTO role(id,role) VALUES(1,'ROLE_ADMIN');
 INSERT INTO role(id,role) VALUES(2,'ROLE_USER');
